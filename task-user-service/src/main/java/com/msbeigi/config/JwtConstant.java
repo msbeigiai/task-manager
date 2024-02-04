afcd8175c6af926822f2dcff8f37a2461f1d7f0e
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-02-04 22:37:11 +0200

    Initial commit

---
+++ task-user-service/src/main/java/com/msbeigi/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.msbeigi.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "eebefieubwubdcb9938rr4h8eivcbsihci0-ncinJXX0D-DKxjjlxjjndjnjs;a'mxc";
+    public static final String SECRET_HEADER = "Authorization";
+
+}
